3038848b6a0a2d4219239c06f56c2224e5d75784
wangjinbiao1985 <42571215+wangjinbiao1985@users.noreply.github.com>
2020-06-17 13:46:55 +0800

    Update UpdateStartBoardcaseReceiver.java

--- android/src/main/java/com/wangjinbiao/flutterqfsilentinstall/UpdateStartBoardcaseReceiver.java
+++ android/src/main/java/com/wangjinbiao/flutterqfsilentinstall/UpdateStartBoardcaseReceiver.java
@@ -5,20 +5,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import static com.wangjinbiao.flutterqfsilentinstall.FlutterQfSilentInstallPlugin.rootStartApk;
+
 /**
  * Created by 24179 on 2017/11/9. 不允许更改
  */
 
 public class UpdateStartBoardcaseReceiver extends BroadcastReceiver {
    private static final String Action =  "android.intent.action.PACKAGE_REPLACED";
+    LogUtil logUtil = new LogUtil(FlutterQfSilentInstallPlugin.class, true);
     @Override
     public void onReceive(Context context, Intent intent) {
         String packageName = intent.getData().getSchemeSpecificPart();
         Log.w("replacePackage",packageName+">-<"+context.getPackageName());
         if(intent.getAction().equals(Action)  && packageName.equals(context.getPackageName())){ //广播和包名确认
-            Intent activity = new Intent(context,MainActivity.class);
-            activity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            context.startActivity(activity);
+//            Intent activity = new Intent(context,MainActivity.class);
+//            activity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//            context.startActivity(activity);
+                      if(rootStartApk("com.wangjinbiao.batterytest", "MainActivity")){
+            logUtil.d("静默安装后启动APP成功");
+          }else{
+            logUtil.e("静默安装后启动APP失败！！！");
+            //Toast.makeText(this,"静默安装后启动APP失败！！！",Toast.LENGTH_SHORT).show();
+          }
         }
     }
 }
